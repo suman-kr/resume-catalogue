0819af0cbc22e065c72c9707d7b58d7829e0592e
Suman Kumar <skcool.123bgp@gmail.com>
2021-04-04 15:56:51 +0530

    Add resume

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Resume from './Resume/Resume';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Resume />
   </React.StrictMode>,
   document.getElementById('root')
 );
