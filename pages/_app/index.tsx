97f6086416c784d7ebed5ec5ef69bfe6c071882c
Suman Kumar <skcool.123bgp@gmail.com>
2021-05-17 00:17:57 +0530

    Fixed build issue

--- pages/_app/index.tsx
+++ pages/_app/index.tsx
@@ -1,19 +1,25 @@
-import './style.scss';
+import "./style.scss";
 
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
-  
-  // Only uncomment this method if you have blocking data requirements for
-  // every single page in your application. This disables the ability to
-  // perform automatic static optimization, causing every page in your app to
-  // be server-side rendered.
-  //
-  // MyApp.getInitialProps = async (appContext) => {
-  //   // calls page's `getInitialProps` and fills `appProps.pageProps`
-  //   const appProps = await App.getInitialProps(appContext);
-  //
-  //   return { ...appProps }
-  // }
-  
-  export default MyApp
+function MyApp(props: MyAppProps) {
+  const { Component, pageProps } = props;
+  return <Component {...pageProps} />;
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+interface MyAppProps {
+  Component: any;
+  pageProps: any;
+}
+
+export default MyApp;
